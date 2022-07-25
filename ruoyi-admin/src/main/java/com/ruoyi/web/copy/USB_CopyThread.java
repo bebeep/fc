0747ffc03d754ade66014fae33d43c50f1eb4a88
hebaosheng <7889082+anchoraqq@user.noreply.gitee.com>
2022-07-25 14:17:56 +0800

    调用grpc

--- ruoyi-admin/src/main/java/com/ruoyi/web/copy/USB_CopyThread.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/copy/USB_CopyThread.java
@@ -51,7 +51,7 @@ public class USB_CopyThread extends Thread {
 
                 System.out.println("总共"+g.size()+"已复制"+ getAllFile(path3,false).size());
                 double progress = (getAllFile(path3,false).size()*1.0)/(g.size() * 1.0);
-                callBackMap.put("progress",String.format("%.2f",progress));
+                callBackMap.put("progress",String.format("%.2f",Math.min(progress,99.99)));
                 callBackMap.put("status",progress>=100?"数据拷贝完成!":"正在拷贝数据...");
                 callBackMap.put("msgType","2");//1打包图像数据 2拷贝文件
                 WebSocketServer.sendInfo(JSONObject.toJSON(callBackMap).toString(),"");
